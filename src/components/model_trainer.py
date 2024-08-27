c44e089e7bca4e37d75b22291f44968dc4970cc0
NoorAmer0 <noorfayyad123@gmail.com>
2024-08-27 15:25:46 +0300

    Hyperparameters tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,8 +43,52 @@ class ModelTrainer:
                 "AdaBoost": AdaBoostRegressor(),
             }
 
+            params = {
+                "Random Forest": {
+                    #Specifies the function to measure the quality of a split.
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256] #number of decision trees
+                },
+                "Decision Tree": {
+                    #Specifies the function to measure the quality of a split.
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+
+                },
+                "Gradient Boosting": {
+                    #Specifies the function to measure the quality of a split.
+                    'criterion':['squared_error', 'friedman_mse'],
+                    #number of features in each tree
+                    'max_features':[None,'sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256],#number of boosting stages or trees to be used.
+                    #which loss function used to calculate the error
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                     #lr Determines the contribution of each tree on the final outcome and controls how quickly the algorithm proceeds down the gradient descent (learns); 
+                    'learning_rate':[.1,.01,.05,.001],
+                    #percentage of subsampled data in each tree
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9]
+                },
+                "Linear Regression": {},
+                "K-Neighbors":{},
+                "XGBoost": {
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate':[.1,.01,.05,.001],
+                },
+                "CatBoost": {
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost": {
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
             # Evaluate models
-            model_report = evaluate_models(X_train, y_train, X_test, y_test, models)
+            model_report = evaluate_models(X_train, y_train, X_test, y_test, models, params)
 
             # Find the best model name and score using max() with a key argument
             best_model_name, best_model_score = max(model_report.items(), key=lambda item: item[1])
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -25,12 +26,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, parameters):
     try:
         report = {}
 
         for i, (model_name, model) in enumerate(models.items()):
-            model.fit(X_train, y_train) 
+            model_parameters = parameters[model_name]
+            gs = GridSearchCV(model, model_parameters, cv=3)
+            gs.fit(X_train, y_train) #fit model with defferent hyperparameters to find the best 
+            
+            model.set_params(**gs.best_params_) #** unpack the dictionary as keyword arguments key=value
+            
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
